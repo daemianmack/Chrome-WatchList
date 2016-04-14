80f2bf858ad88e82bf8e195ad0a45861d383986e
Daemian Mack <daemianmack@gmail.com>
2016-04-14 08:31:19 -0400

    Refactor nodes ns.

--- src/content_script/chromex_sample/content_script/core.cljs
+++ src/content_script/chromex_sample/content_script/core.cljs
@@ -3,7 +3,7 @@
                    [content-script.chromex-sample.content-script.macros :refer [inspect]])
   (:require [reagent.core :as reagent]
             [goog.dom]
-            [clojure.string :as s]
+            [chromex-sample.content-script.nodes :as nodes]
             [re-frame.core :refer [register-handler
                                    register-sub
                                    dispatch
@@ -14,67 +14,12 @@
  (fn [db _]
    (reaction @db)))
 
-(defn node-seq []
-  (let [tree (.createTreeWalker js/document
-                                (.-body js/document)
-                                NodeFilter.SHOW_TEXT)]
-    (take-while some? (repeatedly #(.nextNode tree)))))
-
-(defn text-node [text] (.createTextNode js/document text))
-
-(defn mark-text-node [text]
-  (.createDom goog.dom "mark"
-              (clj->js {:class "watchlist-highlight"})
-              (.createTextNode goog.dom text)))
-
-(defn text [s] {:type :text :body s})
-(defn html [s] {:type :html :body s})
-
-(defn text-gap [matches s start end]
-  (if (< start end)
-    (conj matches (text (subs s start end)))
-    matches))
-
-(defn replace-matches
-  [re s]
-  ;; Needs to make its own g regexp?
-  (reduce 
-   (fn [{:keys [matches prev-idx] :as acc} [term _ :as match]] 
-     (if term
-       (-> acc
-           (update :matches text-gap s prev-idx (.-index match))
-           (update :matches conj (html term))
-           (assoc  :prev-idx (+ (.-index match) (count term))))
-       (reduced (text-gap matches s prev-idx (count s))))) 
-   {:matches [] :prev-idx 0} 
-   (repeatedly #(.exec re s))))
-
-(defn new-nodes
-  [{:keys [type body] :as instructions}]
-  (case type
-    :text {:node (text-node body)}
-    :html {:node (mark-text-node body) :term body}))
 
 (register-handler
  :initialize
  (fn [db [_ option-data]]
-   (let [terms   (js/RegExp. (:terms option-data))
-         terms-g (js/RegExp. (:terms option-data) "g")]
-     (let [nodes   (filterv #(re-find terms (.-textContent %)) (node-seq))
-           _ (.log js/console (str "nodes found" (count nodes)))
-           new-kids (doall (for [node nodes
-                                 :let [content (.-textContent node)]]
-                             [node (map new-nodes (replace-matches terms-g content))]))
-           _ (do (doseq [[node new-kid-seq] new-kids
-                         kid new-kid-seq
-                         :let [parent (.-parentNode node)]]
-                   (.insertBefore parent (:node kid) node))
-                 (doseq [[node _] new-kids]
-                   (.removeChild (.-parentNode node) node)))
-           matches (doall (for [mark (filter :term (mapcat second new-kids))]
-                            {:term (str (:term mark)) :node (.-parentNode (:node mark))}))]
-       (merge db {:started (.getTime (js/Date.))
-                  :matches matches})))))
+   (let [matches (nodes/highlight-matches! (:terms option-data))]
+     (assoc db :matches matches))))
 
 (defn display-match [group-term hits started]
   [:span {:class "watchlist-status-bar-item"
---
+++ src/content_script/chromex_sample/content_script/nodes.cljs
@@ -0,0 +1,60 @@
+(ns chromex-sample.content-script.nodes)
+
+(defn text-objs []
+  (let [tree (.createTreeWalker js/document
+                                (.-body js/document)
+                                NodeFilter.SHOW_TEXT)]
+    (take-while some? (repeatedly #(.nextNode tree)))))
+
+(defn text [s] {:text s})
+(defn html [s] {:html s})
+
+(defn text-gap [matches s start end]
+  (if (< start end)
+    (conj matches (text (subs s start end)))
+    matches))
+
+(defn mark-matches
+  [re s]
+  (reduce 
+   (fn [{:keys [matches prev-idx] :as acc} [term _ :as match]] 
+     (if term
+       (-> acc
+           (update :matches text-gap s prev-idx (.-index match))
+           (update :matches conj (html term))
+           (assoc  :prev-idx (+ (.-index match) (count term))))
+       (reduced (text-gap matches s prev-idx (count s))))) 
+   {:matches [] :prev-idx 0} 
+   (repeatedly #(.exec re s))))
+
+(defn mk-text-node [text] (.createTextNode js/document text))
+
+(defn mk-mark-node [text]
+  (.createDom goog.dom "mark"
+              (clj->js {:class "watchlist-highlight"})
+              (mk-text-node text)))
+
+(defn mk-node
+  [{:keys [text html] :as node-desc}]
+  (cond-> node-desc
+    text (assoc :node (mk-text-node text))
+    html (assoc :node (mk-mark-node html))))
+
+(defn swap-in-nodes!
+  [old-node new-nodes]
+  (doseq [new-node new-nodes
+          :let [parent (.-parentNode old-node)]]
+    (.insertBefore parent (:node new-node) old-node))
+  (.removeChild (.-parentNode old-node) old-node))
+
+(defn highlight-matches!
+  [terms]
+  (let [regex (js/RegExp. terms "g")
+        matching-texts (filterv #(re-find regex (.-textContent %)) (text-objs))
+        new-nodes (for [old-node matching-texts
+                        :let [new-node-descs (mark-matches regex (.-textContent old-node))
+                              new-node-seq   (map mk-node new-node-descs)
+                              _ (swap-in-nodes! old-node new-node-seq)]
+                        mark (filter :html new-node-seq)]
+                    {:term (:html mark) :node (:node mark)})]
+    new-nodes))
