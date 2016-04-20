5022805b21d9ea36971045b92708d011815285d0
Daemian Mack <daemianmack@gmail.com>
2016-04-19 20:00:59 -0400

    Simplify cljsbuilds declaration.

--- project.clj
+++ project.clj
@@ -6,41 +6,25 @@
 
   :plugins [[lein-cljsbuild "1.1.1"]]
 
-  :source-paths ["src"]
+  ;; :source-paths ["src"]
 
   :clean-targets ^{:protect false} ["target"
-                                    "resources/unpacked/compiled"
-                                    "resources/release/compiled"]
+                                    "resources/unpacked/compiled"]
 
-  :cljsbuild {:builds {}}                                                                                                     ; prevent https://github.com/emezeske/lein-cljsbuild/issues/413
+  :cljsbuild {:builds
+              {:watchlist {:source-paths ["src/watchlist" "src/extension"]
+                           :compiler     {:output-to             "resources/unpacked/compiled/watchlist.js"
+                                          :output-dir            "resources/unpacked/compiled/watchlist"
+                                          :asset-path            "compiled/watchlist"
+                                          :optimizations         :whitespace
+                                          :main                  "watchlist.core"
+                                          :anon-fn-naming-policy :unmapped
+                                          :pretty-print          true
+                                          :compiler-stats        true
+                                          :cache-analysis        true
+                                          :source-map            "resources/unpacked/compiled/watchlist.js.map"
+                                          :source-map-timestamp  true}}}}
 
-  :profiles {:unpacked
-             {:cljsbuild {:builds
-                          {:watchlist
-                           {:source-paths ["src/watchlist" "src/extension"]
-                            :compiler     {:output-to             "resources/unpacked/compiled/watchlist.js"
-                                           :output-dir            "resources/unpacked/compiled/watchlist"
-                                           :asset-path            "compiled/watchlist"
-                                           :optimizations         :whitespace
-                                           :main                  "watchlist.core"
-                                           :anon-fn-naming-policy :unmapped
-                                           :pretty-print          true
-                                           :compiler-stats        true
-                                           :cache-analysis        true
-                                           :source-map            "resources/unpacked/compiled/watchlist.js.map"
-                                           :source-map-timestamp  true}}}}}
-             :release
-             {:env       {:chromex-elide-verbose-logging "true"}
-              :cljsbuild {:builds
-                          {:content-script
-                           {:source-paths ["src/content_script"]
-                            :compiler     {:output-to      "resources/release/compiled/content_script.js"
-                                           :output-dir     "resources/release/compiled/content_script"
-                                           :asset-path     "compiled/content_script"
-                                           :optimizations  :advanced
-                                           :elide-asserts  true
-                                           :compiler-stats true}}}}}}
-
-  :aliases {"dev"     ["with-profile" "+unpacked" "cljsbuild" "auto" "watchlist"]
-            "release" ["with-profile" "+release" "do" "clean," "cljsbuild" "once" "watchlist"]
+  :aliases {"dev"     ["cljsbuild" "auto" "watchlist"]
+            "release" ["do" "clean," "cljsbuild" "once" "watchlist"]
             "package" ["shell" "scripts/package.sh"]})
