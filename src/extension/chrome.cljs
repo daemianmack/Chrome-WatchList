89de6f542537989726ab2f5d420d803cc5f8dfee
Daemian Mack <daemianmack@gmail.com>
2016-05-06 09:55:35 -0400

    Document extension ns' reason for existence.

--- src/extension/chrome.cljs
+++ src/extension/chrome.cljs
@@ -1,4 +1,7 @@
 (ns extension.chrome
+  "This being a separate namespace abstracts Chrome storage
+  implementation details out of the init! fn, and hides them from the
+  tests, which don't have a js/chrome object available by default"
   (:require [watchlist.core :as core]))
 
 (.get js/chrome.storage.sync "watchlist"
